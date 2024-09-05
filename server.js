6300a8533bb91c05d81a8028f7fbbd190c322c3f
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2024-09-05 15:31:18 +0530

    Cors error fixed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', '*'],
+  origin: ['http://localhost:5173', '*', 'https://dhr-bmi.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
